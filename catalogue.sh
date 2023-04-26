5d1799110e5e40a9f93cfe69e991cb12504f5683
sunil <sunilkumar.ipm@gmail.com>
2023-04-26 13:11:15 +0530

    changes 3

--- catalogue.sh
+++ catalogue.sh
@@ -30,7 +30,7 @@ systemctl enable catalogue
 systemctl restart catalogue
 
 echo -e "\e[36m>>>>>>>>>>copy Mongo.repo file<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/Project-1/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>>>>>>>Install MongoD<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
