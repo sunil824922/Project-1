c831b9b890eb8dae3729006f491ade8425243716
sunil <sunilkumar.ipm@gmail.com>
2023-04-27 12:13:44 +0530

    listen address update

--- redis.sh
+++ redis.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>>>>>>Install remirepo<<<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>>>>>enable redis<<<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[36m>>>>>>>>>>install redis<<<<<<<<<<\e[0m"
 yum install redis -y
-#  address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+
+echo -e "\e[36m>>>>>>>>>>update redis listen address<<<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.0|0.0.0.0| /etc/redis.conf'
+
+echo -e "\e[36m>>>>>>>>>>restart redis<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis
