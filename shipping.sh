048852f804055e72719bc1126400d10b0bbc974c
sunil <sunilkumar.ipm@gmail.com>
2023-04-28 17:11:09 +0530

    shipping 1

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,11 @@
 echo -e "\e[36m>>>>>>>>>>Install maven<<<<<<<<<<\e[0m"
 yum install maven -y
 
+echo -e "\e[36m>>>>>>>>>>Create app user<<<<<<<<<<\e[0m"
+useradd roboshop
+
 echo -e "\e[36m>>>>>>>>>>create app user<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
 echo -e "\e[36m>>>>>>>>>>Download APP content<<<<<<<<<<\e[0m"
@@ -15,12 +19,14 @@ echo -e "\e[36m>>>>>>>>>>Download maven dependencies<<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-echo -e "\e[36m>>>>>>>>>>copy the content<<<<<<<<<<\e[0m"
-cp /home/centos/Project-1/shipping.service /etc/systemd/system/shipping.service
-
 echo -e "\e[36m>>>>>>>>>>install Mysql<<<<<<<<<<\e[0m"
 yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>>>download schema<<<<<<<<<<\e[0m"
 mysql -h mysql-dev.devops2023sk.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>>>setup systemd service<<<<<<<<<<\e[0m"
+cp /home/centos/Project-1/shipping.service /etc/systemd/system/shipping.service
 
 echo -e "\e[36m>>>>>>>>>>restart the shipping<<<<<<<<<<\e[0m"
 systemctl daemon-reload
