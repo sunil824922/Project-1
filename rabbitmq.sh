7c1c5a652edf2f8af8f79fef4e0d925f4fd461a7
sunil <sunilkumar.ipm@gmail.com>
2023-04-29 12:58:10 +0530

    RabbitMQ

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,11 +1,14 @@
 echo -e "\e[36m>>>>>>>>>>setup erlang repositories<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
+echo -e "\e[36m>>>>>>>>>>install erlang<<<<<<<<<<\e[0m"
+yum install erlang -y
+
 echo -e "\e[36m>>>>>>>>>>setup RabbitMQ Repos<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
 echo -e "\e[36m>>>>>>>>>>Install erlang & RabbitMQ server<<<<<<<<<<\e[0m"
-yum install erlang rabbitmq-server -y
+yum install rabbitmq-server -y
 
 echo -e "\e[36m>>>>>>>>>>Start RabbitMQ server<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
