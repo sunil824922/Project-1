714172bab23d9d7effe0e5b61b13647eb3520213
sunil <sunilkumar.ipm@gmail.com>
2023-04-30 12:56:07 +0530

    variables

--- variable.sh
+++ variable.sh
@@ -1,9 +1,11 @@
 a=10
 Name=sunil
-DATE=$(date +%F%T)
+DATE=$(date +%F)
 
 #Print Variables
 echo a = $a
 echo name = ${Name}
 echo Today date & TIME is ${DATE}
 
+arth=$((2+6+6/12))
+echo  arthimetic=${arth}
