30e6066dc46e21f04237b4a3886998cf5f53cb4f
sunil <sunilkumar.ipm@gmail.com>
2023-05-03 22:45:41 +0530

    functions

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 
 print_head() {
-  echo -e "\e[35m>>>>>>>>>>$1<<<<<<<<<<\e[0m"
+  echo -e "\e[34m>>>>>>>>>>$1<<<<<<<<<<\e[0m"
 }
 
 func_nodejs() {
