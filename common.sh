5fb95dd5989c46eb6afc295daa0427f8ecd14e50
sunil <sunilkumar.ipm@gmail.com>
2023-05-03 22:41:51 +0530

    functions

--- common.sh
+++ common.sh
@@ -2,37 +2,37 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head() {echo -e "\e[35m>>>>>>>>>>$1<<<<<<<<<<\e[0m"
+func_print_head() {echo -e "\e[35m>>>>>>>>>>$1<<<<<<<<<<\e[0m"
 }
 
 func_nodejs() {
-print_head "Configiure Nodejs repos"
+func_print_head "Configiure Nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-print_head "Install Nodejs"
+func_print_head "Install Nodejs"
 yum install nodejs -y
 
-print_head "Add application user"
+func_print_head "Add application user"
 useradd ${app_user}
 
-print_head  "Create application directory"
+func_print_head  "Create application directory"
 rm -rf /app
 mkdir /app
 
-print_head "Download app content"
+func_print_head "Download app content"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 cd /app
 
-print_head "Unzip user"
+func_print_head "Unzip user"
 unzip /tmp/${component}.zip
 
-print_head "Install Nodejs dependencies"
+func_print_head "Install Nodejs dependencies"
 npm install
 
-print_head "Copy usersystemD file"
+func_print_head "Copy usersystemD file"
 cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-print_head "restart service"
+func_print_head "restart service"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl restart ${component}
