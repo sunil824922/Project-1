10129ce41129ad90f81c7f0e745de02ce5781415
sunil <sunilkumar.ipm@gmail.com>
2023-05-04 17:20:32 +0530

    shipping

--- common.sh
+++ common.sh
@@ -2,10 +2,10 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head() {
+func_print_head() {
   echo -e "\e[34m>>>>>>>>>>$1<<<<<<<<<<\e[0m"
 }
-schema_setup() {
+func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
 
   print_head "copy Mongo.repo file"
@@ -18,42 +18,75 @@ schema_setup() {
   mongo --host mongodb-dev.devops2023sk.online </app/schema/catalogue.js
 
   fi
+  if [ "${schema_setup}"== "mysql" ]; then
+
+  func_print_head "install Mysql"
+  yum install mysql -y
+
+  func_print_head "download schema"
+  mysql -h mysql-dev.devops2023sk.online -uroot -p$(mysql_root_password) < /app/schema/shipping.sql
 }
 
+func_app_prereq() {
+  func_print_head "Add application user"
+  useradd ${app_user}
+
+  func_print_head "Create application directory"
+  rm -rf /app
+  mkdir /app
+
+  func_print_head "Download application content"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+
+  func_print_head "Extract application conetent"
+  unzip /tmp/${component}.zip
+}
+
+func_systemd_setup() {
+  func_print_head "setup systemd service"
+  cp $script_path/${component}.service /etc/systemd/system/${component}.service
+
+  func_print_head "start the ${component} service"
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl restart ${component}
+
+
+}
 
 func_nodejs() {
-print_head "Configiure Nodejs repos"
+func_print_head "Configiure Nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-print_head "Install Nodejs"
+func_print_head "Install Nodejs"
 yum install nodejs -y
 
-print_head "Add application user"
-useradd ${app_user}
+func_app_prereq
 
-print_head  "Create application directory"
-rm -rf /app
-mkdir /app
-
-print_head "Download app content"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-cd /app
-
-print_head "Unzip user"
-unzip /tmp/${component}.zip
-
-print_head "Install Nodejs dependencies"
+func_print_head "Install Nodejs dependencies"
 npm install
 
-print_head "Copy usersystemD file"
-cp $script_path/${component}.service /etc/systemd/system/${component}.service
+func_schema_setup
 
-print_head "restart service"
-systemctl daemon-reload
-systemctl enable ${component}
-systemctl restart ${component}
+func_systemd_setup
+
+}
+
+func_java() {
+func_print_head "Install maven"
+yum install maven -y
+
+func_app_prereq
+
+func_print_head "Download maven dependencies"
+mvn clean package
+mv target/${component}-1.0.jar ${component}.jar
+
+func_schema_setup
+
+func_systemd_setup
 
 
-schema_setup
 }
 
--- shipping.sh
+++ shipping.sh
@@ -8,37 +8,6 @@ if [ -z "$mysql_root_password" ]; then
   exit 1
 fi
 
-print_head "Install maven"
-yum install maven -y
-
-print_head 'Create app user'
-useradd roboshop
-
-print_head "create app user"
-rm -rf /app
-mkdir /app
-
-print_head "Download APP content"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-
-print_head "Extract app content"
-cd /app
-unzip /tmp/shipping.zip
-
-print_head "Download maven dependencies"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-print_head "install Mysql"
-yum install mysql -y
-
-print_head "download schema"
-mysql -h mysql-dev.devops2023sk.online -uroot -p$(mysql_root_password) < /app/schema/shipping.sql
-
-print_head "setup systemd service"
-cp $script_path/shipping.service /etc/systemd/system/shipping.service
-
-print_headre "start the shipping"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+component="shipping"
+schema_setup=mysql
+func_java
