64d00948c704681ad13faec71863a161e34381ab
sunil <sunilkumar.ipm@gmail.com>
2023-05-11 16:24:14 +0530

    terraform

--- cart.sh
+++
@@ -1,7 +0,0 @@
-script=$(realpath "$0")
-script_path=$(dirname "$script")
-source ${script_path}/common.sh
-
-component=cart
-
-func_nodejs
--- user.sh
+++
@@ -1,9 +0,0 @@
-script=$(realpath "$0")
-script_path=$(dirname "$script")
-source ${script_path}/common.sh
-
-component=user
-schema_setup=mongo
-
-func_nodejs
-
