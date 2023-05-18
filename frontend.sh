94a2853db2fc005bef2e6ba02777b6176061b6e3
sunil <sunilkumar.ipm@gmail.com>
2023-05-18 17:01:39 +0530

    roboshop terrafrom

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+
 func_print_head "Install nginx"
 yum install nginx -y  &>>$log_file
 func_stat_check $?
